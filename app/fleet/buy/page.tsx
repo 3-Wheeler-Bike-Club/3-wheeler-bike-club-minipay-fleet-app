834a6ee07c8a7f5a73a1f9e4db0a8223c4d65b92
Tickether <tickether@gmail.com>
2025-04-24 23:42:58 +0000

    feat: add cool doodles

--- app/fleet/buy/page.tsx
+++ app/fleet/buy/page.tsx
@@ -3,7 +3,7 @@ import { Wrapper } from "@/components/fleet/buy/wrapper";
 export default async function Buy() {
     return (
 
-        <main className="flex w-screen h-screen">
+        <main className="flex w-screen h-screen bg-[url('/images/dodo.svg')] bg-center bg-cover">
                 <Wrapper/>
         </main>
     )
