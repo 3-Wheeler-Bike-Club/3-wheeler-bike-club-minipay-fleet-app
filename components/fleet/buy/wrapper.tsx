a0842a0585022a38403f3a3a9a84a66c301f5a2e
Tickether <tickether@gmail.com>
2025-04-27 17:43:43 +0000

    feat: USDT approval button gradient bg
    + hover

--- components/fleet/buy/wrapper.tsx
+++ components/fleet/buy/wrapper.tsx
@@ -371,7 +371,7 @@ export function Wrapper() {
                             <div className="flex w-full justify-between">
                                 {/**pay with USDT */}
                                 <Button 
-                                    className="w-48/100" 
+                                    className={`${allowanceUSDT && allowanceUSDT > 0 ? "w-48/100" : "w-48/100 bg-gradient-to-r from-yellow-400 to-green-500"}`} 
                                     disabled={loadingCeloUSD || loadingUSDT} 
                                     onClick={() => {
                                         if (allowanceUSDT && allowanceUSDT > 0) {
@@ -417,7 +417,7 @@ export function Wrapper() {
                                 </Button>
                                 {/**pay with celoUSD */}
                                 <Button 
-                                    className={` ${allowanceCeloUSD && allowanceCeloUSD > 0 ? "w-48/100" : "w-48/100 bg-gradient-to-r from-yellow-400 to-green-500"}`}
+                                    className={` ${allowanceCeloUSD && allowanceCeloUSD > 0 ? "w-48/100" : "w-48/100 bg-gradient-to-r from-yellow-400 to-green-500"}` }
                                     disabled={loadingCeloUSD || loadingUSDT} 
                                     onClick={() => {
                                         if (allowanceCeloUSD && allowanceCeloUSD > 0) {
