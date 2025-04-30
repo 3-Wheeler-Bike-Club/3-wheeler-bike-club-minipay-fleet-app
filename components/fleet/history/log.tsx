bad72f8960fc9c0c7759cd7cfb3b8b81d5e31468
Tickether <tickether@gmail.com>
2025-04-30 23:51:28 +0000

    feat: update contract

--- components/fleet/history/log.tsx
+++ components/fleet/history/log.tsx
@@ -17,7 +17,7 @@ export function Log({ log }: LogProps) {
     return (
         <>
             <TableRow>
-                <TableCell>{log.args.fleetId}</TableCell>
+                <TableCell>{log.args.id}</TableCell>
                 <TableCell>{log.eventName === "FleetOrdered" ? <div className="flex items-center gap-2"><RefreshCw className="h-4 w-4 text-yellow-600"/> <p>1</p></div> : <div className="flex items-center gap-2"><ChartPie className="h-4 w-4 text-yellow-600"/> <p className="text-xs">{log.args.fractions} / 50</p></div>}</TableCell>
                 <TableCell>{shortenTxt(log.transactionHash)}</TableCell>
                 <TableCell>{new Date(Number(blockTime) * 1000).toLocaleDateString()}</TableCell>
