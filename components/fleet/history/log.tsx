2a129b05483b88cddb41b87c850c25330bd8dad2
Tickether <tickether@gmail.com>
2025-05-07 17:42:20 +0000

    feat: make history scrollable

--- components/fleet/history/log.tsx
+++ components/fleet/history/log.tsx
@@ -16,10 +16,10 @@ export function Log({ log }: LogProps) {
 
     return (
         <>
-            <TableRow>
-                <TableCell>{log.eventName === "FleetOrdered" ? log.args.ids.join(", ") : log.args.id}</TableCell>
-                <TableCell>{log.eventName === "FleetOrdered" ? <div className="flex items-center gap-2"><RefreshCw className="h-4 w-4 text-yellow-600"/> <p>{log.args.ids.length}</p></div> : <div className="flex items-center gap-2"><ChartPie className="h-4 w-4 text-yellow-600"/> <p className="text-xs">{log.args.fractions} / 50</p></div>}</TableCell>
-                <TableCell>
+            <TableRow className="flex w-full">
+                <TableCell className="w-1/4">{log.eventName === "FleetOrdered" ? log.args.ids.join(", ") : log.args.id}</TableCell>
+                <TableCell className="w-1/4">{log.eventName === "FleetOrdered" ? <div className="flex items-center gap-2"><RefreshCw className="h-4 w-4 text-yellow-600"/> <p>{log.args.ids.length}</p></div> : <div className="flex items-center gap-2"><ChartPie className="h-4 w-4 text-yellow-600"/> <p className="text-xs">{log.args.fractions} / 50</p></div>}</TableCell>
+                <TableCell className="w-1/4">
                     <a 
                         href={`https://celoscan.io/tx/${log.transactionHash}`}
                         target="_blank"
@@ -29,7 +29,7 @@ export function Log({ log }: LogProps) {
                         {shortenTxt(log.transactionHash)}
                     </a>
                 </TableCell>
-                <TableCell>{new Date(Number(blockTime) * 1000).toLocaleDateString()}</TableCell>
+                <TableCell className="w-1/4">{new Date(Number(blockTime) * 1000).toLocaleDateString()}</TableCell>
             </TableRow>
         </>
     )
