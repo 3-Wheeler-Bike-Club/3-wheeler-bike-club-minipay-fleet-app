661091d094f9d6a8267882b3f78c14721f711630
Tickether <tickether@gmail.com>
2025-05-09 08:50:41 +0000

    feat: mainnet to OP + cleanup divvi

--- utils/divvi.ts
+++ utils/divvi.ts
@@ -1,11 +1,11 @@
 import { getDataSuffix, submitReferral } from '@divvi/referral-sdk'
 import { createWalletClient, custom } from 'viem'
-import { mainnet } from 'viem/chains'
+import { optimism } from 'viem/chains'
 import { divvi } from './constants/addresses'
 
 // Step 1: Create a wallet client and get the account
 export const walletClient = createWalletClient({
-  chain: mainnet,
+  chain: optimism,
   transport: custom(window.ethereum),
 })
 const [account] = await walletClient.getAddresses()
@@ -15,7 +15,7 @@ const [account] = await walletClient.getAddresses()
 // consumer is your Divvi Identifier
 // providers are the addresses of the Rewards Campaigns that you signed up for on the previous page
 const dataSuffix = getDataSuffix({
-  consumer: '0x123',
+  consumer: '0x99342D3CE2d10C34b7d20D960EA75bd742aec468',
   providers: ['0x5f0a55FaD9424ac99429f635dfb9bF20c3360Ab8','0xB06a1b291863f923E7417E9F302e2a84018c33C5','0x6226ddE08402642964f9A6de844ea3116F0dFc7e'],
 })
   
@@ -28,11 +28,11 @@ const txHash = await walletClient.sendTransaction({
 })
 
 // Step 3: Get the chain ID of the chain that the transaction was sent to
-const chainId = await walletClient.getChainId()
+//const chainId = await walletClient.getChainId()
 
 // Step 4: Report the transaction to the attribution tracking API
 await submitReferral({
   txHash,
-  chainId,
+  chainId: optimism.id,
 })
     
