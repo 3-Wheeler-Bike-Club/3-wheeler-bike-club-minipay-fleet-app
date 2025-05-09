908938f8d5a8f7bf7574667adac334c10d712214
Tickether <tickether@gmail.com>
2025-05-09 09:11:59 +0000

    feat: cleanup divvi hook

--- hooks/useDivvi.tsx
+++ hooks/useDivvi.tsx
@@ -3,36 +3,40 @@ import { getDataSuffix, submitReferral } from '@divvi/referral-sdk'
 import { createWalletClient, custom } from 'viem'
 import { optimism } from 'viem/chains'
 
-// Step 1: Create a wallet client and get the account
-export const walletClient = createWalletClient({
-  chain: optimism,
-  transport: custom(window.ethereum),
-})
-const [account] = await walletClient.getAddresses()
-
-// Step 2: Execute an existing transaction within your codebase with the referral data suffix
-
-// consumer is your Divvi Identifier
-// providers are the addresses of the Rewards Campaigns that you signed up for on the previous page
-const dataSuffix = getDataSuffix({
-  consumer: '0x99342D3CE2d10C34b7d20D960EA75bd742aec468',
-  providers: ['0x5f0a55FaD9424ac99429f635dfb9bF20c3360Ab8','0xB06a1b291863f923E7417E9F302e2a84018c33C5','0x6226ddE08402642964f9A6de844ea3116F0dFc7e'],
-})
+export const useDivvi = (account: `0x${string}`) => {
   
-const txHash = await walletClient.sendTransaction({
-  account,
-  to: divvi,
-  data: "0x" + dataSuffix as `0x${string}`,
-  value: BigInt(0),
-  // ... other transaction parameters
-})
+    async function registerUser() {
+      // Step 1: Create a wallet client and get the account
+      const walletClient = createWalletClient({
+        chain: optimism,
+        transport: custom(window.ethereum),
+      })
 
-// Step 3: Get the chain ID of the chain that the transaction was sent to
-//const chainId = await walletClient.getChainId()
+      // Step 2: Execute an existing transaction within your codebase with the referral data suffix
 
-// Step 4: Report the transaction to the attribution tracking API
-await submitReferral({
-  txHash,
-  chainId: optimism.id,
-})
-    
+      // consumer is your Divvi Identifier
+      // providers are the addresses of the Rewards Campaigns that you signed up for on the previous page
+      const dataSuffix = getDataSuffix({
+        consumer: '0x99342D3CE2d10C34b7d20D960EA75bd742aec468',
+        providers: ['0x5f0a55FaD9424ac99429f635dfb9bF20c3360Ab8','0xB06a1b291863f923E7417E9F302e2a84018c33C5','0x6226ddE08402642964f9A6de844ea3116F0dFc7e'],
+      })
+      
+      const txHash = await walletClient.sendTransaction({
+        account,
+        to: divvi,
+        data: "0x" + dataSuffix as `0x${string}`,
+        value: BigInt(0),
+        // ... other transaction parameters
+      })
+
+      // Step 3: Get the chain ID of the chain that the transaction was sent to
+      //const chainId = await walletClient.getChainId()
+
+      // Step 4: Report the transaction to the attribution tracking API
+      submitReferral({
+        txHash,
+        chainId: optimism.id,
+      })          
+    }
+  
+}
