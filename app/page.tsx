9c95f1eec743b23f4e5c9f7ba9b82f69a42934e5
Tickether <tickether@gmail.com>
2025-05-12 15:56:01 +0000

    chore: minor bg fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Wrapper } from "@/components/landing/wrapper";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col items-center justify-center h-screen">
+    <div className="relative flex flex-col items-center justify-center h-full">
         <div
           className="
             absolute inset-0
